0042ed2d30b43d595f6434dfc7312d750b6d8569
Colin Mackay <github@colinmackay.scot>
2019-11-14 23:01:36 +0000

    Realign M74 through Glasgow

--- src/helpers/renderLabel.js
+++ src/helpers/renderLabel.js
@@ -8,38 +8,39 @@ module.exports = function (plop) {
       return new Handlebars.SafeString("");
     }
     const labelOffsetX = junction.labelOffsetX ? junction.labelOffsetX : 0;
+    const labelOffsetY = junction.labelOffsetY ? junction.labelOffsetY : 0;
     let rendered = "\n<!-- Label: --> ";
     switch (junction.labelOffset) {
       case "n": {
-        rendered += `<text x="${junction.x + labelOffsetX}" y="${junction.y - (standard.cellSize * 1.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="middle" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX}" y="${junction.y - (standard.cellSize * 1.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="middle" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "ne": {
-        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y - (standard.cellSize * 1.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y - (standard.cellSize * 1.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "e": {
-        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y - standard.cellHalfSize}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y - standard.cellHalfSize + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "se": {
-        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y + (standard.cellSize * 0.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX + (standard.cellSize * 0.6)}" y="${junction.y + (standard.cellSize * 0.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="start" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "s": {
-        rendered += `<text x="${junction.x + labelOffsetX}" y="${junction.y + (standard.cellSize * 0.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="middle" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX}" y="${junction.y + (standard.cellSize * 0.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="middle" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "sw": {
-        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y + (standard.cellSize * 0.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y + (standard.cellSize * 0.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "w": {
-        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y - standard.cellHalfSize}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y - standard.cellHalfSize + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       case "nw": {
-        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y - (standard.cellSize * 1.6)}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
+        rendered += `<text x="${junction.x + labelOffsetX - (standard.cellSize * 0.6)}" y="${junction.y - (standard.cellSize * 1.6) + labelOffsetY}" font-family="Fira Sans Condensed" dominant-baseline="hanging" text-anchor="end" font-size="${standard.cellSize}px">${junction.name}</text>`;
         break;
       }
       default: {
