e9b27e8e0bec989daa6b062c5717b408d5764404
Colin Mackay <github@colinmackay.scot>
2019-11-15 18:39:18 +0000

    Move the route data into a json file outside the template

--- src/helpers/setData.js
+++ src/helpers/setData.js
@@ -1,20 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const log = require("./common/log");
+
 module.exports = function (plop) {
-  plop.handlebars.registerHelper("setData", function (name, options) {
-    // console.log("setData CALL:");
-    // console.log("name:");
-    // console.log(name);
-    // console.log("");
-    // console.log("options:");
-    // console.log(options);
-    // console.log("");
-    const text = options.fn("");
-    // console.log("Text:");
-    // console.log(text);
-    // console.log("");
+  plop.handlebars.registerHelper("setData", function (name, location, options) {
+    let text = "";
+    if (location === "inline") {
+      text = options.fn("");
+    } else {
+      const absoluteLocation = path.resolve(location);
+      log(`Looking for the "${name}" data at "${absoluteLocation}"`);
+      text = fs.readFileSync(absoluteLocation, "utf8");
+    }
     const obj = JSON.parse(text);
-    // console.log("JSON:");
-    // console.log(obj);
-    // console.log("");
     options.data.root[name] = obj;
   });
 };
