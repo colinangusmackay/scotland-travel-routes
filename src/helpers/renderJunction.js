c7d366410572a03763aee96926ddd6a981759f2a
Colin Mackay <github@colinmackay.scot>
2019-11-24 14:17:04 +0000

    Render as dotted junctions

--- src/helpers/renderJunction.js
+++ src/helpers/renderJunction.js
@@ -4,61 +4,75 @@ const getRoute = require("./common/getRoute");
 const Handlebars = require("handlebars");
 const log = require("./common/log");
 
+function renderStandardJunction (junction, route) {
+  let rendered = "\n<!-- Junction: --> ";
+  const path = [{ x: junction.x, y: junction.y }];
+  switch (junction.labelOffset) {
+    case "n": {
+      path.push({ x: junction.x, y: junction.y - standard.cellHalfSize });
+      break;
+    }
+    case "ne": {
+      path.push({ x: junction.x + (standard.cellHalfSize * standard.cos45), y: junction.y - (standard.cellHalfSize * standard.sin45) });
+      break;
+    }
+    case "e": {
+      path.push({ x: junction.x + standard.cellHalfSize, y: junction.y });
+      break;
+    }
+    case "se": {
+      path.push({ x: junction.x + standard.cellHalfSize, y: junction.y + standard.cellHalfSize });
+      break;
+    }
+    case "s": {
+      path.push({ x: junction.x, y: junction.y + standard.cellHalfSize });
+      break;
+    }
+    case "sw": {
+      path.push({ x: junction.x - (standard.cellHalfSize * standard.cos45), y: junction.y + (standard.cellHalfSize * standard.sin45) });
+      break;
+    }
+    case "w": {
+      path.push({ x: junction.x - standard.cellHalfSize, y: junction.y });
+      break;
+    }
+    case "nw": {
+      path.push({ x: junction.x - standard.cellHalfSize, y: junction.y - standard.cellHalfSize });
+      break;
+    }
+    default: {
+      log(`Unexpected junction marker direction, "${junction.labelOffset}", on "${route.name}.${junction.number}".`);
+      break;
+    }
+  }
+  if (path.length > 1) {
+    rendered += generatePolyline(path, {
+      stroke: route.colour,
+      fill: "none",
+      "stroke-width": standard.lineWidth,
+      "stroke-linecap": "butt"
+    });
+  }
+  return rendered;
+}
+
+function renderStandardDottedJunction (junction, route) {
+  let rendered = "\n<!-- Junction: --> ";
+  rendered += `<circle cx="${junction.x}" cy="${junction.y}" r="${standard.lineWidth / 2}" fill="white" stroke="black" stroke-width="${standard.lineWidth / 5}" />`;
+  return rendered;
+}
+
 module.exports = function (plop) {
   plop.addHelper("renderJunction", function (junction, context) {
     const route = getRoute(context);
 
     let rendered = "";
     if ((junction.type === "standard") && (junction.labelOffset)) {
-      rendered += "\n<!-- Junction: --> ";
-      const path = [{ x: junction.x, y: junction.y }];
-      switch (junction.labelOffset) {
-        case "n": {
-          path.push({ x: junction.x, y: junction.y - standard.cellHalfSize });
-          break;
-        }
-        case "ne": {
-          path.push({ x: junction.x + (standard.cellHalfSize * standard.cos45), y: junction.y - (standard.cellHalfSize * standard.sin45) });
-          break;
-        }
-        case "e": {
-          path.push({ x: junction.x + standard.cellHalfSize, y: junction.y });
-          break;
-        }
-        case "se": {
-          path.push({ x: junction.x + standard.cellHalfSize, y: junction.y + standard.cellHalfSize });
-          break;
-        }
-        case "s": {
-          path.push({ x: junction.x, y: junction.y + standard.cellHalfSize });
-          break;
-        }
-        case "sw": {
-          path.push({ x: junction.x - (standard.cellHalfSize * standard.cos45), y: junction.y + (standard.cellHalfSize * standard.sin45) });
-          break;
-        }
-        case "w": {
-          path.push({ x: junction.x - standard.cellHalfSize, y: junction.y });
-          break;
-        }
-        case "nw": {
-          path.push({ x: junction.x - standard.cellHalfSize, y: junction.y - standard.cellHalfSize });
-          break;
-        }
-        default: {
-          log(`Unexpected junction marker direction, "${junction.labelOffset}", on "${context.data._parent.key}.${context.data.key}".`);
-          break;
-        }
-      }
-      if (path.length > 1) {
-        rendered += generatePolyline(path, {
-          stroke: route.colour,
-          fill: "none",
-          "stroke-width": standard.lineWidth,
-          "stroke-linecap": "butt"
-        });
-      }
+      rendered = renderStandardJunction(junction, route);
+    } else if ((junction.type === "dotted-junction")) {
+      rendered = renderStandardDottedJunction(junction, route);
     }
+
     return new Handlebars.SafeString(rendered);
   });
 };
--- src/helpers/renderTerminator.js
+++ src/helpers/renderTerminator.js
@@ -3,74 +3,89 @@ const standard = require("./common/standardConsts");
 const getRoute = require("./common/getRoute");
 const log = require("./common/log");
 
+function renderBlockTerminator (junction, route) {
+  const width = standard.lineWidth;
+  const height = standard.cellSize;
+
+  let rendered = "\n<!-- Terminator: --> ";
+  switch (junction.angle) {
+    // TODO: Diagonal offsets
+    case "ew": {
+      const tlx = junction.x - (width / 2);
+      const tly = junction.y - (height / 2);
+      rendered += `<rect x="${tlx}" y="${tly}" width="${width}" height="${height}" fill="${route.colour}" />`;
+      break;
+    }
+    case "ns": {
+      const tlx = junction.x - (height / 2);
+      const tly = junction.y - (width / 2);
+      rendered += `<rect x="${tlx}" y="${tly}" width="${height}" height="${width}" fill="${route.colour}" />`;
+      break;
+    }
+    default: {
+      console.log(`Unexpected junction angle, "${junction.angle}", on "${context.data._parent.key}.${context.data.key}".`);
+      break;
+    }
+  }
+
+  switch (junction.terminatorLabelOffset) {
+    // TODO: Add the other offsets
+    case null:
+    case undefined:
+    case "":
+      break;
+    case "n": {
+      const labelX = junction.x;
+      const labelY = junction.y - standard.cellHalfSize;
+      const text = route.name;
+      rendered += `<text x="${labelX}" y="${labelY}" text-anchor="middle" dominant-baseline="baseline" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
+      break;
+    }
+    case "e": {
+      const labelX = junction.x + width;
+      const labelY = junction.y;
+      const text = route.name;
+      rendered += `<text x="${labelX}" y="${labelY}" dominant-baseline="middle" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
+      break;
+    }
+    case "s": {
+      const labelX = junction.x;
+      const labelY = junction.y + standard.cellHalfSize;
+      const text = route.name;
+      rendered += `<text x="${labelX}" y="${labelY}" text-anchor="middle" dominant-baseline="hanging" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
+      break;
+    }
+    case "w":
+    {
+      const labelX = junction.x - width;
+      const labelY = junction.y;
+      const text = route.name;
+      rendered += `<text x="${labelX}" y="${labelY}" text-anchor="end" dominant-baseline="middle" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
+      break;
+    }
+    default: {
+      log(`Unexpected terminator label offset, "${junction.terminatorLabelOffset}", on "${route.name}.${junction.number}".`);
+    }
+  }
+  return rendered;
+}
+
+function renderDottedTerminator (junction, route) {
+  return `<circle cx="${junction.x}" cy="${junction.y}" r="${standard.lineWidth / 2}" fill="black" stroke="white" stroke-width="${standard.lineWidth / 10}" />`;
+}
+
 module.exports = function (plop) {
   plop.addHelper("renderTerminator", function (junction, context) {
-    if (junction.type === "terminator") {
-      const width = 20;
-      const height = 50;
-      const route = getRoute(context);
-
-      let rendered = "\n<!-- Terminator: --> ";
-      switch (junction.angle) {
-        // TODO: Diagonal offsets
-        case "ew": {
-          const tlx = junction.x - (width / 2);
-          const tly = junction.y - (height / 2);
-          rendered += `<rect x="${tlx}" y="${tly}" width="${width}" height="${height}" fill="${route.colour}" />`;
-          break;
-        }
-        case "ns": {
-          const tlx = junction.x - (height / 2);
-          const tly = junction.y - (width / 2);
-          rendered += `<rect x="${tlx}" y="${tly}" width="${height}" height="${width}" fill="${route.colour}" />`;
-          break;
-        }
-        default: {
-          console.log(`Unexpected junction angle, "${junction.angle}", on "${context.data._parent.key}.${context.data.key}".`);
-          break;
-        }
-      }
-
-      switch (junction.terminatorLabelOffset) {
-        // TODO: Add the other offsets
-        case null:
-        case undefined:
-        case "":
-          break;
-        case "n": {
-          const labelX = junction.x;
-          const labelY = junction.y - standard.cellHalfSize;
-          const text = route.name;
-          rendered += `<text x="${labelX}" y="${labelY}" text-anchor="middle" dominant-baseline="baseline" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
-          break;
-        }
-        case "e": {
-          const labelX = junction.x + width;
-          const labelY = junction.y;
-          const text = route.name;
-          rendered += `<text x="${labelX}" y="${labelY}" dominant-baseline="middle" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
-          break;
-        }
-        case "s": {
-          const labelX = junction.x;
-          const labelY = junction.y + standard.cellHalfSize;
-          const text = route.name;
-          rendered += `<text x="${labelX}" y="${labelY}" text-anchor="middle" dominant-baseline="hanging" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
-          break;
-        }
-        case "w":
-        {
-          const labelX = junction.x - width;
-          const labelY = junction.y;
-          const text = route.name;
-          rendered += `<text x="${labelX}" y="${labelY}" text-anchor="end" dominant-baseline="middle" font-family="Fira Sans Condensed" font-size="50px">${text}</text>`;
-          break;
-        }
-        default: {
-          log(`Unexpected terminator label offset, "${junction.terminatorLabelOffset}", on "${context.data._parent.key}.${context.data.key}".`);
-        }
-      }
-      return new Handlebars.SafeString(rendered);
+    const route = getRoute(context);
+    let rendered = "";
+    switch (junction.type) {
+      case "terminator":
+        rendered = renderBlockTerminator(junction, route);
+        break;
+      case "dotted-terminator":
+        rendered = renderDottedTerminator(junction, route);
+        break;
     }
+    return new Handlebars.SafeString(rendered);
   });
 };
